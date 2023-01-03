f52c34df35f7451616d528a514e5e8868bcd4a5e
soumensardar <soumen.sardar@smithsdetection.com>
2023-01-03 17:52:05 +0530

    in progress-fasterrcnn added

--- model/fmcgnet.py
+++ model/fmcgnet.py
@@ -72,6 +72,75 @@ class InitialBlock(nn.Module):
         return self.out_activation(out)
 
 
+class TerminalBlock(nn.Module):
+    """The terminal block is composed of two heads:
+    1. a bounding box regression head
+    2. a object classification head
+
+    Doing both operations in parallel and concatenating their results
+    allows for efficient downsampling and expansion. The main branch
+    outputs 13 feature maps while the extension branch outputs 3, for a
+    total of 16 feature maps after concatenation.
+
+    Keyword arguments:
+    - in_channels (int): the number of input channels.
+    - out_channels (int): the number output channels.
+    - kernel_size (int, optional): the kernel size of the filters used in
+    the convolution layer. Default: 3.
+    - padding (int, optional): zero-padding added to both sides of the
+    input. Default: 0.
+    - bias (bool, optional): Adds a learnable bias to the output if
+    ``True``. Default: False.
+    - relu (bool, optional): When ``True`` ReLU is used as the activation
+    function; otherwise, PReLU is used. Default: True.
+
+    """
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 bias=False,
+                 relu=True):
+        super().__init__()
+
+        if relu:
+            activation = nn.ReLU
+        else:
+            activation = nn.PReLU
+
+        # Main branch - As stated above the number of output channels for this
+        # branch is the total minus 3, since the remaining channels come from
+        # the extension branch
+        self.main_branch = nn.Conv2d(
+            in_channels,
+            out_channels - 3,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            bias=bias)
+
+        # Extension branch
+        self.ext_branch = nn.MaxPool2d(3, stride=2, padding=1)
+
+        # Initialize batch normalization to be used after concatenation
+        self.batch_norm = nn.BatchNorm2d(out_channels)
+
+        # PReLU layer to apply after concatenating the branches
+        self.out_activation = activation()
+
+    def forward(self, x):
+        main = self.main_branch(x)
+        ext = self.ext_branch(x)
+
+        # Concatenate branches
+        out = torch.cat((main, ext), 1)
+
+        # Apply batch normalization
+        out = self.batch_norm(out)
+
+        return self.out_activation(out)
+
+
 class RegularBottleneck(nn.Module):
     """Regular bottlenecks are the main building block of ENet.
     Main branch:
