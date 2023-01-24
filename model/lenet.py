e91ff14c73db5b5b2fd2b8a83f91badc29e9e629
soumensardar <soumen.sardar@smithsdetection.com>
2023-01-24 11:54:29 +0530

    - lenet code cleanup

--- model/lenet.py
+++ model/lenet.py
@@ -56,11 +56,6 @@ class LeNet(nn.Module):
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=ps)  # non-learning layer, we can reuse
         self.conv3 = conv(in_channels=f2, out_channels=f3, kernel_size=k3, stride=s3, padding=p3,
                           dilation=d3)
-        self.conv4 = conv(in_channels=f3, out_channels=f4, kernel_size=k4, stride=s4, padding=p4,
-                          dilation=d4)
-        self.conv5 = conv(in_channels=f4, out_channels=f5, kernel_size=k5, stride=s5, padding=p5,
-                          dilation=d5)
-
 
         # As original LeNet designed for 32x32
         # images patches, we need to calculate
@@ -72,11 +67,10 @@ class LeNet(nn.Module):
         x = self.pool(self.activation(self.conv1(x)))
         x = self.pool(self.activation(self.conv2(x)))
         x = self.activation(self.conv3(x))
-        x = self.activation(self.conv4(x))
-        x = self.activation(self.conv5(x))
-        self.fc_fx_size = x.shape[1] * x.shape[2] * x.shape[3]
+        fc_fx_size = x.shape[1] * x.shape[2] * x.shape[3]
 
-        self.fc1 = nn.Linear(self.fc_fx_size, fc1o)
+        self.flatten = nn.Flatten(start_dim=1)
+        self.fc1 = nn.Linear(fc_fx_size, fc1o)
         self.fc2 = nn.Linear(fc1o, fc2o)
         self.fc3 = nn.Linear(fc2o, num_classes)
 
@@ -100,9 +94,7 @@ class LeNet(nn.Module):
         x = self.activation(self.conv2(x))
         x = self.pool(x)
         x = self.activation(self.conv3(x))
-        x = self.activation(self.conv4(x))
-        x = self.activation(self.conv5(x))
-        x = x.view(-1, self.fc_fx_size)  # flatten(but batch wise)
+        x = self.flatten(x)  # flatten(but batch wise)
         x = self.activation(self.fc1(x))
         x = self.activation(self.fc2(x))
         x = self.activation(self.fc3(x))
@@ -128,124 +120,131 @@ class LeNet(nn.Module):
         return x
 
 
+# ######################################################################################
+# helper function to show an image
+# (used in the `plot_classes_preds` function below)
+def matplotlib_imshow(img, one_channel=False):
+    if one_channel:
+        img = img.mean(dim=0)
+    img = cv2.normalize(img.numpy(), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+    # img = img / 2 + 0.5  # unnormalize
+    # npimg = img.numpy()
+    if one_channel:
+        plt.imshow(img, cmap="Greys")
+    else:
+        plt.imshow(np.transpose(img, (1, 2, 0)))
+
+
+def images_to_probs(net, images):
+    '''
+    Generates predictions and corresponding probabilities from a trained
+    network and a list of images
+    '''
+    output = net(images)
+    # convert output probabilities to predicted class
+    _, preds_tensor = torch.max(output, 1)
+    preds = np.squeeze(preds_tensor.numpy())
+    return preds, [F.softmax(el, dim=0)[i].item() for i, el in zip(preds, output)]
+
+
+def plot_classes_preds(net, images, labels, classes):
+    '''
+    Generates matplotlib Figure using a trained network, along with images
+    and labels from a batch, that shows the network's top prediction along
+    with its probability, alongside the actual label, coloring this
+    information based on whether the prediction was correct or not.
+    Uses the "images_to_probs" function.
+    '''
+    preds, probs = images_to_probs(net, images)
+    # plot the images in the batch, along with predicted and true labels
+    fig = plt.figure(figsize=(12, 48))
+    for idx in np.arange(4):
+        ax = fig.add_subplot(1, 4, idx + 1, xticks=[], yticks=[])
+        matplotlib_imshow(images[idx], one_channel=False)
+        ax.set_title("{0}, {1:.1f}%\n(label: {2})".format(
+            classes[preds[idx]],
+            probs[idx] * 100.0,
+            classes[labels[idx]]),
+            color=("green" if preds[idx] == labels[idx].item() else "red"))
+    return fig
+
+
+def compute_multiclass_auc(epoch_idx, num_epochs, model, test_loader, meta_dict, detailed_output=False):
+    print("\nTesting... epoch:[%d/%d]" % (epoch_idx + 1, num_epochs))
+    model.eval()
+    labels_all, probs_all = [], []
+    with torch.no_grad():
+        for (images, labels), meta in tqdm(test_loader, leave=False, total=len(test_loader)):
+            logits = model(images)
+            probs = torch.softmax(logits, dim=-1)
+            probs_all.append(probs.numpy())
+            labels_all.append(labels.numpy())
+    labels_all = np.concatenate(labels_all)
+    probs_all = np.concatenate(probs_all)
+    n_classes = probs_all.shape[1]
+    auc_list = []
+    auc_dict = defaultdict(lambda: [])
+    for class_idx in tqdm(range(n_classes), leave=False):
+        labels_binary = (labels_all == class_idx).astype(int)
+        try:
+            auc = roc_auc_score(labels_binary, probs_all[:, class_idx])
+        except ValueError:
+            # this happens when a batch contains negative or positive samples as ground_truth
+            auc = 0.0
+        auc_list.append(auc)
+        auc_dict[class_idx].append(auc)
+    mean_auc = np.mean(auc_list)
+    for key, values in auc_dict.items():
+        auc_dict[key] = np.mean(values)
+    model.train()
+    if detailed_output:
+        return mean_auc, auc_list, auc_dict
+    else:
+        return mean_auc
+
+
+def train_one_batch(model, images, labels, optimizer, criterion_class_loss):
+    optimizer.zero_grad()
+    # bboxes_pred, logits_pred = net(inputs)
+    logits_pred = model(images)  # forward
+    # calculate the loss for bounding box regression
+    # bbox_loss_value = criterion_bbox_loss(bboxes_pred, bboxes)
+    # calculate the loss for classification
+    class_loss_value = criterion_class_loss(logits_pred, labels)
+    # combine the loss
+    # loss = bbox_loss_value + class_loss_value
+    loss = class_loss_value
+    # backprop
+    loss.backward()
+    # update weights
+    optimizer.step()
+    return loss
+
+
+def train_one_epoch(opt, writer, epoch_idx, num_epoch, model, meta_dict, train_loader, optimizer, criterion_class_loss):
+    print("\nTraining... epoch:[%d/%d]" % (epoch_idx + 1, num_epoch))
+    model.train()  # activate training mode
+    meta_dict["train_running_loss"] = 0.0
+    for batch_idx, data in enumerate(tqdm(train_loader, leave=False, total=len(train_loader))):
+        meta_dict["step_ctr"] += opt.batchsize
+        (inputs, targets), meta = data
+        loss = train_one_batch(model, inputs, targets, optimizer, criterion_class_loss)
+        # create grid of images
+        img_grid = torchvision.utils.make_grid(inputs)
+        # write to tensorboard
+        writer.add_image('Batch Images', img_grid)
+        meta_dict["train_running_loss"] += loss.item()
+        meta_dict["batch_ctr"] += 1
+        if meta_dict["batch_ctr"] % opt.print_freq == 0:
+            # log the running loss
+            writer.add_scalar('training loss', meta_dict["train_running_loss"] / opt.print_freq,
+                              meta_dict["step_ctr"])
+            meta_dict["total_train_loss"] = (meta_dict["train_running_loss"] / opt.print_freq)
+            meta_dict["train_running_loss"] = 0.0
+    print("Training Loss:", meta_dict["total_train_loss"], "Epoch:", epoch_idx + 1)
+
+
 def train_loop(opt, classes, writer, train_loader, test_loader, val_loader):
-    # helper function to show an image
-    # (used in the `plot_classes_preds` function below)
-    def matplotlib_imshow(img, one_channel=False):
-        if one_channel:
-            img = img.mean(dim=0)
-        img = cv2.normalize(img.numpy(), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-        # img = img / 2 + 0.5  # unnormalize
-        # npimg = img.numpy()
-        if one_channel:
-            plt.imshow(img, cmap="Greys")
-        else:
-            plt.imshow(np.transpose(img, (1, 2, 0)))
-
-    def images_to_probs(net, images):
-        '''
-        Generates predictions and corresponding probabilities from a trained
-        network and a list of images
-        '''
-        output = net(images)
-        # convert output probabilities to predicted class
-        _, preds_tensor = torch.max(output, 1)
-        preds = np.squeeze(preds_tensor.numpy())
-        return preds, [F.softmax(el, dim=0)[i].item() for i, el in zip(preds, output)]
-
-    def plot_classes_preds(net, images, labels, classes):
-        '''
-        Generates matplotlib Figure using a trained network, along with images
-        and labels from a batch, that shows the network's top prediction along
-        with its probability, alongside the actual label, coloring this
-        information based on whether the prediction was correct or not.
-        Uses the "images_to_probs" function.
-        '''
-        preds, probs = images_to_probs(net, images)
-        # plot the images in the batch, along with predicted and true labels
-        fig = plt.figure(figsize=(12, 48))
-        for idx in np.arange(4):
-            ax = fig.add_subplot(1, 4, idx + 1, xticks=[], yticks=[])
-            matplotlib_imshow(images[idx], one_channel=False)
-            ax.set_title("{0}, {1:.1f}%\n(label: {2})".format(
-                classes[preds[idx]],
-                probs[idx] * 100.0,
-                classes[labels[idx]]),
-                color=("green" if preds[idx] == labels[idx].item() else "red"))
-        return fig
-
-    def compute_multiclass_auc(epoch_idx, num_epochs, model, test_loader, meta_dict, detailed_output=False):
-        print("\nTesting... epoch:[%d/%d]" % (epoch_idx + 1, num_epochs))
-        model.eval()
-        labels_all, probs_all = [], []
-        with torch.no_grad():
-            for (images, labels), meta in tqdm(test_loader, leave=False, total=len(test_loader)):
-                logits = model(images)
-                probs = torch.softmax(logits, dim=-1)
-                probs_all.append(probs.numpy())
-                labels_all.append(labels.numpy())
-        labels_all = np.concatenate(labels_all)
-        probs_all = np.concatenate(probs_all)
-        n_classes = probs_all.shape[1]
-        auc_list = []
-        auc_dict = defaultdict(lambda: [])
-        for class_idx in tqdm(range(n_classes), leave=False):
-            labels_binary = (labels_all == class_idx).astype(int)
-            try:
-                auc = roc_auc_score(labels_binary, probs_all[:, class_idx])
-            except ValueError:
-                # this happens when a batch contains negative or positive samples as ground_truth
-                auc = 0.0
-            auc_list.append(auc)
-            auc_dict[class_idx].append(auc)
-        mean_auc = np.mean(auc_list)
-        for key, values in auc_dict.items():
-            auc_dict[key] = np.mean(values)
-        model.train()
-        if detailed_output:
-            return mean_auc, auc_list, auc_dict
-        else:
-            return mean_auc
-
-    def train_one_batch(model, images, labels):
-        optimizer.zero_grad()
-        # bboxes_pred, logits_pred = net(inputs)
-        logits_pred = model(images)  # forward
-        # calculate the loss for bounding box regression
-        # bbox_loss_value = criterion_bbox_loss(bboxes_pred, bboxes)
-        # calculate the loss for classification
-        class_loss_value = criterion_class_loss(logits_pred, labels)
-        # combine the loss
-        # loss = bbox_loss_value + class_loss_value
-        loss = class_loss_value
-        # backprop
-        loss.backward()
-        # update weights
-        optimizer.step()
-        return loss
-
-    def train_one_epoch(epoch_id, num_epoch, model, meta_dict):
-        print("\nTraining... epoch:[%d/%d]" % (epoch_idx + 1, num_epochs))
-        net.train()  # activate training mode
-        meta_dict["train_running_loss"] = 0.0
-        for batch_idx, data in enumerate(tqdm(train_loader, leave=False, total=len(train_loader))):
-            meta_dict["step_ctr"] += opt.batchsize
-            (inputs, targets), meta = data
-            loss = train_one_batch(model, inputs, targets)
-            # create grid of images
-            img_grid = torchvision.utils.make_grid(inputs)
-            # write to tensorboard
-            writer.add_image('Batch Images', img_grid)
-            meta_dict["train_running_loss"] += loss.item()
-            meta_dict["batch_ctr"] += 1
-            if meta_dict["batch_ctr"] % opt.print_freq == 0:
-                # log the running loss
-                writer.add_scalar('training loss', meta_dict["train_running_loss"] / opt.print_freq,
-                                  meta_dict["step_ctr"])
-                meta_dict["total_train_loss"] = (meta_dict["train_running_loss"] / opt.print_freq)
-                meta_dict["train_running_loss"] = 0.0
-        print("Training Loss:", meta_dict["total_train_loss"], "Epoch:", epoch_id + 1)
-
     # Define the number of training epochs
     num_epochs = opt.niter
 
@@ -297,7 +296,8 @@ def train_loop(opt, classes, writer, train_loader, test_loader, val_loader):
         )
 
         # train
-        train_one_epoch(epoch_idx, num_epochs, net, meta_dict)
+        train_one_epoch(opt, writer, epoch_idx, num_epochs, net, meta_dict, train_loader, optimizer,
+                        criterion_class_loss)
 
         # test
         detailed = True
